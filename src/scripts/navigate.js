8f4772a58275fb22c018420a3c0ebbce03bbc9b7
Paul Ehrling <ghostlest330@gmail.com>
2024-04-29 20:03:50 +0200

    Refactor navigate.js to use spawn instead of exec and improve error handling

--- src/scripts/navigate.js
+++ src/scripts/navigate.js
@@ -1,8 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
-import process from 'process';
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 
 function setWorkingDirectory(projectName) {
     const userDirectory = os.homedir();
@@ -14,21 +13,17 @@ function setWorkingDirectory(projectName) {
         const config = JSON.parse(rawData);
 
         if (config[projectName]) {
-            const path = stringify(config[projectName])
-            console.log(config[projectName])
-            exec(`powershell.exe -File ./src/pw-scripts/changeDir.ps1 ${JSON.stringify(config[projectName])}`, (error, stdout, stderr) => {
-                console.log(stdout);
-                console.log(stderr)
-                if (error) {
-                    console.error(`Project not found`);
-                    return;
-                }
-
-                if(stderr) {
-                    console.error("Project not found");
-                    return;
-                }
-            })
+            console.log('Starting directory: ' + process.cwd());
+            try {
+                process.chdir(config[projectName]);
+                console.log('New directory: ' + process.cwd());
+                const newTerminal = spawn('powershell', ['-Command', 'Start-Process', 'powershell'], { shell: true });
+                newTerminal.on('error', (error) => {
+                    console.error(`spawn error: ${error}`);
+                });
+            } catch (err) {
+                console.error('chdir: ' + err);
+            }
         } else {
             throw new Error ('Project not found');
         }
