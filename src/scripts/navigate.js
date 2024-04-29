b8bbfb829a023f44c98eeab00df311543ab2649d
Paul Ehrling <ghostlest330@gmail.com>
2024-04-29 19:18:13 +0200

    Refactor navigate.js to remove unnecessary imports and fix project path

--- src/scripts/navigate.js
+++ src/scripts/navigate.js
@@ -1,12 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
-<<<<<<< HEAD
 import process from 'process';
-=======
 import { exec } from 'child_process';
-import { stringify } from 'querystring';
->>>>>>> e23f3817f3093785f3d9bcdf9dbbbbe23f1611d3
 
 function setWorkingDirectory(projectName) {
     const userDirectory = os.homedir();
@@ -18,11 +14,8 @@ function setWorkingDirectory(projectName) {
         const config = JSON.parse(rawData);
 
         if (config[projectName]) {
-<<<<<<< HEAD
-            process.chdir("C:\\Users\\Ehrling\\RouteRacoon");
-=======
             const path = stringify(config[projectName])
-             console.log(config[projectName])
+            console.log(config[projectName])
             exec(`powershell.exe -File ./src/pw-scripts/changeDir.ps1 ${JSON.stringify(config[projectName])}`, (error, stdout, stderr) => {
                 console.log(stdout);
                 console.log(stderr)
@@ -35,9 +28,7 @@ function setWorkingDirectory(projectName) {
                     console.error("Project not found");
                     return;
                 }
-
             })
->>>>>>> e23f3817f3093785f3d9bcdf9dbbbbe23f1611d3
         } else {
             throw new Error ('Project not found');
         }
